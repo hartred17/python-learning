9565b8d61bf470dd8b8daebaf2da65d22ef7be2d
hartred17 <vtemnote@nana.local>
2026-06-17 14:52:17 +0300

    Remove gpt.py

--- gpt.py
+++
@@ -1,7 +0,0 @@
-servers = [
-    {"name": "VPN Server", "country": "Netherlands"},
-    {"name": "Bot Server", "country": "Germany"}
-]
-
-for server in servers:
-    print(f'{server["name"]} - {server["country"]}')
