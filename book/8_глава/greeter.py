87d3ad3e50d6e0d1a90d52708a1ebffbe00e885a
hartred17 <vtemnote@nana.local>
2026-07-16 15:15:05 +0300

    greeter.py

--- book/8_глава/greeter.py
+++ book/8_глава/greeter.py
@@ -1,5 +1,13 @@
-def greet_user(username):
-    """Выводит простое приветствие."""
-    print(f"Hello, {username.title()}!")
+def get_formatted_name(first_name, last_name):
+    """Возвращает отформатированное полное имя."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
-greet_user('jesse')
+# Бесконечный цикл!
+while True:
+    print("\nPlease tell me your name")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
